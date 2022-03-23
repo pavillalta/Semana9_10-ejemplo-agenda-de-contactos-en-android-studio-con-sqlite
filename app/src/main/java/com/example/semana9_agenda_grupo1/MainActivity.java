127c34caa640fe689c0500aaf5a7213c755d9223
pavillalta <pavblogs@gmail.com>
2022-03-23 09:49:02 -0600

    Actualización

--- app/src/main/java/com/example/semana9_agenda_grupo1/MainActivity.java
+++ app/src/main/java/com/example/semana9_agenda_grupo1/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        obtenerDatos();
     }
 
     @Override
